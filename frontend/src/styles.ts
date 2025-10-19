5326cdd6d1d689a234afb790840c9565aa0ef548
Efedrkoglu <efedurukoglu@gmail.com>
2025-10-19 15:32:55 +0300

    theme.css & styles.ts

---
+++ frontend/src/styles.ts
@@ -0,0 +1,7 @@
+//button styles
+export const btnPrimary = "btn bg-[var(--color-primary)] hover:bg-[var(--color-primary)]/80 rounded-lg border-none text-white";
+export const btnSecondary = "btn bg-[var(--color-secondary)] hover:bg-[var(--color-secondary)]/80 rounded-lg border-none";
+
+//link styles
+export const linkPrimary = "text hover:text-[var(--color-primary)] hover:underline hover:cursor-pointer";
+export const linkSecondary = "text hover:text-[var(--color-secondary)] hover:underline hover:cursor-pointer";
