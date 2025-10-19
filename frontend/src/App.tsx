22311506b0e4f9eb1d87dc38f47e93324c94c996
Efedrkoglu <efedurukoglu@gmail.com>
2025-10-19 14:31:19 +0300

    index layout & navbar & footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import IndexLayout from "./layouts/IndexLayout";
 import Index from "./pages/Index";
+import Contact from "./pages/Contact";
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Index />}></Route>
+        <Route path="/" element={<IndexLayout />}>
+          <Route index element={<Index />}></Route>
+          <Route path="iletisim" element={<Contact />}></Route>
+        </Route>
       </Routes>
     </Router>
   )
