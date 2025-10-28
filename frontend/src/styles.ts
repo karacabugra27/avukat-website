b7260ad5fb90ce261662ef59fbecc5df0c8945c7
Efedrkoglu <efedurukoglu@gmail.com>
2025-10-28 16:47:43 +0300

    navbar update & mobile navbar

--- frontend/src/styles.ts
+++ frontend/src/styles.ts
@@ -3,5 +3,6 @@ export const btnPrimary = "btn bg-[var(--color-primary)] hover:bg-[var(--color-p
 export const btnSecondary = "btn bg-[var(--color-secondary)] hover:bg-[var(--color-secondary)]/80 rounded-lg border-none";
 
 //link styles
+export const navLink = "hover:text-[var(--color-primary)] transition-all duration-200"
 export const linkPrimary = "text hover:text-[var(--color-primary)] hover:underline hover:cursor-pointer";
 export const linkSecondary = "text hover:text-[var(--color-secondary)] hover:underline hover:cursor-pointer";
